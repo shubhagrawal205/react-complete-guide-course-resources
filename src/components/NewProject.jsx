d12bdf0e28ff79dea801c7d7d54c806964afe77c
Shubh Agrawal <shubh.agrawal@ril.com>
2024-05-03 11:00:54 +0530

    ADDED NEW PROJECT AND CREATE NEW PROJECT BUTTON

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,27 @@
 import Input from "./Input";
+import { useRef } from "react";
 
-export default function NewProject(){
+export default function NewProject({onAdd}){
+
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+     function handleSave(){
+        const enteredTitle = title.current.value;
+        const enteredDescription = title.current.value;
+        const enteredDueDate = title.current.value; 
+
+        //Validation
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate,
+        });
+
+
+     }
+
     return(
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
@@ -10,15 +31,16 @@ export default function NewProject(){
                     </button>
                 </li>
                 <li>
-                    <button className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">
+                    <button className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950"
+                    onClick={handleSave}>
                         Save
                     </button>
                 </li>
             </menu>
             <div>
-                <Input label="Title"/>
-                <Input label="Description" textarea/>
-                <Input label="Due date"/>
+                <Input ref={title} label="Title"/>
+                <Input ref={description} label="Description" textarea/>
+                <Input ref={dueDate}  label="Due date"/>
             </div>
         </div>
     )
